d550a5c45f89c2dc39ce170e0c27478d506acf8c
rijal jatnika <jatnika10@gmail.com>
2020-07-14 17:03:54 +0700

    perbaikan di css nya

--- application/views/frontend/templates/header.php
+++ application/views/frontend/templates/header.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="icon" type="image/png" href="<?= base_url('assets/frontend/') ?>/image/logo-merah.png">
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="<?= base_url('assets/frontend/') ?>css/main.css">
+    <link rel="stylesheet" href="<?= base_url('/assets/frontend/') ?>css/main.css">
     <link rel="stylesheet" href="<?= base_url('assets/frontend/') ?>css/bootstrap.css">
     <link rel="stylesheet" href="<?= base_url('assets/frontend/') ?>css/grid-gallery.css">
     <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&display=swap" rel="stylesheet">
