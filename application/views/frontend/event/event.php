623a4f79bec9ef9d2a98e7ecd66d872ffaf10c1d
GARIS PROJECT <59761186+garis-project@users.noreply.github.com>
2020-07-21 06:15:50 +0700

    Back (#8)
    * replace
    
    * Add, View, Update and Delete Stage -Need Fixing Map Geocode
    
    * login and register in frontend
    
    * -
    
    * Fixing Event Error(upload image), Add Event In Frontend

--- application/views/frontend/event/event.php
+++ application/views/frontend/event/event.php
@@ -91,7 +91,3 @@
 
 		</div>
 		<?php }?>
-		<script src="assets/js/jquery.js"></script>
-		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
-		<script src="assets/js/bootstrap.js"></script>
-	</body>
